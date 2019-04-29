fc33f05b46a1b3438cfcfbda921e5d2b40d35a85
Jan W <JanWittke97@web.de>
2019-04-29 20:08:39 +0200

    Developed AnimationComponent and TagSystem

---
+++ PastMinutes/Assets/Scripts/Systems/PartFindingSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Finds the offset for the corresponding characterPart
+/// </summary>
+public static class PartFindingSystem
+{
+    /// <summary>
+    /// Location of bodyparts in spritesheet. Index is first occurence of body part
+    /// </summary>
+   public enum BodyParts
+    {
+        Head = 5,
+        Torso = 10,
+        Arms = 15,
+        Legs = 25,
+        ArmsWithGun = 35
+        
+    }
+}
---
+++ PastMinutes/Assets/Scripts/Systems/TagSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagSystem : MonoBehaviour
+{
+    public bool Camera;
+    public bool Player;
+    public bool Enemy;
+    public bool Item;
+
+
+
+}
