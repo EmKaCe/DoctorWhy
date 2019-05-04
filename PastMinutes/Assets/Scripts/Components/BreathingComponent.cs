41ab856956b04c076fe40d830ecd521489aae07d
Jan W <JanWittke97@web.de>
2019-05-04 18:48:47 +0200

    Added OxygenSystem and BreathingComponent

---
+++ PastMinutes/Assets/Scripts/Components/BreathingComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingComponent : MonoBehaviour
+{
+    public float oxygenUsage;
+    private List<OxygenComponent> oxygenSupply;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddOxygenSupply(OxygenComponent supply)
+    {
+        oxygenSupply.Add(supply);
+    }
+}
