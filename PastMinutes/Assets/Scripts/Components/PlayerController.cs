b84e265c8e8c8ad8ecf955fdbac63542ed8d59ed
Jan W <JanWittke97@web.de>
2019-05-06 12:47:57 +0200

    MovementComponent

--- PastMinutes/Assets/Scripts/Components/PlayerController.cs
+++ PastMinutes/Assets/Scripts/Components/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float movementSpeed;
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        if(rigidbody == null)
+        {
+            Debug.Log("The player character needs a 2d rigidbody");
+            gameObject.GetComponent<PlayerController>().enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 currentPos = rigidbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rigidbody.MovePosition(newPos);
     }
 }
