ae80b4bb07377819973cac8cc429fac402187f6b
Jan W <JanWittke97@web.de>
2019-05-20 19:59:45 +0200

    1st fix for shootingComponents
    1. Remaining bullets in magazine are taken into account
    2. projectile gets spawned at the pivot point of the sprite (needs to be at the tip of the gun, maybe only temporary fix)

--- PastMinutes/Assets/Scripts/Components/GunComponents/GunComponent.cs
+++ PastMinutes/Assets/Scripts/Components/GunComponents/GunComponent.cs
@@ -9,6 +9,8 @@ public class GunComponent : MonoBehaviour
     public MagazineComponent magazine;
     public RailAttachmentComponent rail;
     public MuzzleAttachmentComponent muzzle;
+    public PartFindingSystem.FiringModes firingModes;
+    public PartFindingSystem.CurrentFiringMode currentMode;
 
     // Start is called before the first frame update
     void Start()
--- PastMinutes/Assets/Scripts/Components/ShootingComponent.cs
+++ PastMinutes/Assets/Scripts/Components/ShootingComponent.cs
@@ -8,77 +8,107 @@ using UnityEngine;
 public class ShootingComponent : MonoBehaviour
 {
 
-    private SpriteRenderer spriteRenderer;
-    private GunComponent activeGun;
+    private bool activeGun = true;
     private string projectileSpritesheet;
     private Sprite projectile;
     public bool autoFire;
     private bool shooting;
     private GunComponent gun;
+    private MagazineComponent mag;
+    private AmmunitionComponent ammo;
+
+    private float muzzleVelocity;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         EventManager.TriggerEvent(EventSystem.InitializingShootingComponent(), gameObject.GetComponentInParent<EntityComponent>().gameObject.GetInstanceID(), new string[] { "0" });
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         projectile = Resources.Load<Sprite>("Items/" + projectileSpritesheet);
         Debug.Log(projectile);
         //work in progress: take gun component for projectile information
-        gun = gameObject.GetComponent<GunComponent>() as GunComponent;
+        gun = gameObject.GetComponent<GunComponent>() as GunComponent;       
+        
+        if(gun != null)
+        {
+            muzzleVelocity = gun.muzzleVelocity;
+            mag = gun.magazine;
+            if(mag != null)
+            {
+                ammo = mag.projectile;
+                projectile = ammo.projectile;
+            }
+        }
+        
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (autoFire)
+        if (activeGun)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (autoFire)
             {
-                shooting = true;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //start coroutine
+                    shooting = true;
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    //stop coroutine
+                    shooting = false;
+                }
+                if (shooting)
+                {
+                    Shoot();
+                }
             }
-            if (Input.GetMouseButtonUp(0))
+            else
             {
-                shooting = false;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Shoot();
+                }
             }
-            if (shooting)
-            {
-                Shoot();
-            }
+
         }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Shoot();
-            }
-        }
-        
-        
+
+
     }
 
     public void LoadSprite()
     {
-        projectile = Resources.Load<Sprite>("Items/" + projectileSpritesheet);
+        projectile = ammo.projectile;
     }
 
     public void Shoot()
     {
-        Vector3 mouse = Input.mousePosition;
-        mouse = Camera.main.ScreenToWorldPoint(mouse);
-        GameObject bullet = new GameObject("Projectile");
-        SpriteRenderer r = bullet.AddComponent<SpriteRenderer>();
-        r.sprite = projectile;
-        ProjectileComponent p = bullet.AddComponent<ProjectileComponent>();
-        p.speed = 2;
-        p.seconds = 10;
-        p.direction = new Vector2(mouse.x, mouse.y);
-        p.direction.Normalize();
+        if (mag.amountOfAmunition > 0)
+        {
+            mag.amountOfAmunition--;
+            Vector3 mouse = Input.mousePosition;
+            mouse = Camera.main.ScreenToWorldPoint(mouse);
+            GameObject bullet = new GameObject("Projectile");
+            Debug.Log(gameObject.name);
+            bullet.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -1.5f);
+            SpriteRenderer r = bullet.AddComponent<SpriteRenderer>();
+            r.sprite = projectile;
+            ProjectileComponent p = bullet.AddComponent<ProjectileComponent>();
+            p.speed = muzzleVelocity;
+            p.seconds = 10;
+            p.direction = new Vector2(mouse.x, mouse.y);
+            p.direction.Normalize();
+        }
+        
 
     }
 
-    public void SetActiveGun(GunComponent gun)
+    public void SetActiveGun(bool gun)
     {
         activeGun = gun;
     }
