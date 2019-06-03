b8b1fcafc40c49eec636f9622088b38de45b1bb6
Jan W <JanWittke97@web.de>
2019-06-03 16:52:09 +0200

    A*

--- PastMinutes/Assets/Scripts/PathfingingAlgorithm.cs
+++ PastMinutes/Assets/Scripts/PathfingingAlgorithm.cs
@@ -7,50 +7,118 @@ public class PathfingingAlgorithm
 {
     //https://www.raywenderlich.com/3016-introduction-to-a-pathfinding
     //https://arongranberg.com/astar/
-    List<Vector3Int> openList;
-    List<Vector3Int> closedList;
+    //List<Vector3Int> openList;
+    Dictionary<Vector3Int, float> openList;
+    Dictionary<Vector3Int, float> closedList;
     GridLayout layout;
     Tilemap map;
+    Mask[] mask = new Mask[8];
 
     public class Waypoint
     {
 
     }
 
-
-
-
-    public List<Waypoint> FindPath(Vector3 startPos, Vector3 endPos)
+    public class Mask
     {
+        public int x;
+        public int y;
+        public float weight;
+
+        public Mask(int x, int y, float weight)
+        {
+            this.x = x;
+            this.y = y;
+            this.weight = weight;
+        }
+    }
+
+
+
+
+    public List<Waypoint> FindPath(Vector3 startPos, Vector3 endPos, float startValue)
+    {
+        mask[0] = new Mask(-1, -1, 1.4f);
+        mask[1] = new Mask(-1, 0, 1f);
+        mask[2] = new Mask(-1, 1, 1.4f);
+        mask[3] = new Mask(0, -1, 1f);
+        mask[4] = new Mask(0, 1, 1f);
+        mask[5] = new Mask(1, -1, 1.4f);
+        mask[6] = new Mask(1, 0, 1f);
+        mask[7] = new Mask(1, 1, 1.4f);
+
+
+
+
         List<Waypoint> path = new List<Waypoint>();
         //Tiles to check
-        openList = new List<Vector3Int>();
+        openList = new Dictionary<Vector3Int, float>();
         //Tiles do not need to be checked again
-        closedList = new List<Vector3Int>();
+        closedList = new Dictionary<Vector3Int, float>();
         Vector3Int start = layout.WorldToCell(startPos);
-        closedList.Add(start);
-        openList.Add(new Vector3Int(start.x - 1, start.y -1, start.z));
-        openList.Add(new Vector3Int(start.x, start.y - 1, start.z));
-        openList.Add(new Vector3Int(start.x + 1, start.y + 1, start.z));
-        openList.Add(new Vector3Int(start.x - 1, start.y, start.z));
-        openList.Add(new Vector3Int(start.x + 1, start.y, start.z));
-        openList.Add(new Vector3Int(start.x, start.y, start.z));
-        openList.Add(new Vector3Int(start.x, start.y, start.z));
-        openList.Add(new Vector3Int(start.x, start.y, start.z));
+        openList.Add(start, startValue);
 
-        for(int x = -1; x <= 1; x++)
+
+        return path;
+    }
+
+    private void FindPath()
+    {
+        KeyValuePair<Vector3Int, float> start = FindClosestTile();
+        openList.Remove(start.Key);
+        closedList.Add(start.Key, start.Value);
+        for(int i = 0; i < 8; i++)
         {
-            for(int y = -1; y <= 1; y++)
+            Vector3Int point = new Vector3Int(start.Key.x + mask[i].x, start.Key.y, start.Key.z);
+            if (closedList.ContainsKey(point))
             {
-                Vector3Int tile = new Vector3Int(start.x + x, start.y + y, start.z);
-                TileBase t = map.GetTile(tile);
+                //Do nothing
+            }
+            else if (openList.ContainsKey(point))
+            {
+                //Check Value
+            }
+            else
+            {
+                //Add
             }
         }
 
 
+        //openList.Add(new Vector3Int(start.Key.x - 1, start.Key.y - 1, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x - 1, start.Key.y - 1, start.Key.z), 1.4f));
+        //openList.Add(new Vector3Int(start.Key.x, start.Key.y - 1, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x, start.Key.y - 1, start.Key.z), 1f));
+        //openList.Add(new Vector3Int(start.Key.x + 1, start.Key.y - 1, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x + 1, start.Key.y - 1, start.Key.z), 1.4f));
+        //openList.Add(new Vector3Int(start.Key.x - 1, start.Key.y, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x - 1, start.Key.y, start.Key.z), 1f));
+        //openList.Add(new Vector3Int(start.Key.x + 1, start.Key.y, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x + 1, start.Key.y, start.Key.z), 1f));
+        //openList.Add(new Vector3Int(start.Key.x - 1, start.Key.y + 1, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x - 1, start.Key.y + 1, start.Key.z), 1.4f));
+        //openList.Add(new Vector3Int(start.Key.x, start.Key.y + 1, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x, start.Key.y + 1, start.Key.z), 1f));
+        //openList.Add(new Vector3Int(start.Key.x + 1, start.Key.y + 1, start.Key.z), CalcF(start.Key, new Vector3Int(start.Key.x + 1, start.Key.y + 1, start.Key.z), 1.4f));
+    }
 
+    private KeyValuePair<Vector3Int, float> FindClosestTile()
+    {
+        Vector3Int pos = new Vector3Int();
+        float distance = 10000;
+        foreach(KeyValuePair<Vector3Int, float> pair in openList)
+        {
+            if(pair.Value < distance)
+            {
+                distance = pair.Value;
+                pos = pair.Key;
+            }
+        }
+        return new KeyValuePair<Vector3Int, float>(pos, distance);
+    }
 
-        return path;
+    private float CalcG(Vector3Int start, Vector3Int end)
+    {
+        return Mathf.Abs(Mathf.Sqrt(Mathf.Pow(end.x - start.x, 2) + Mathf.Pow(end.y - start.y, 2)));
+    }
+
+    private float CalcF(Vector3Int start, Vector3Int end, float endWeight)
+    {
+        openList.TryGetValue(start, out float startValue);
+        return startValue + CalcG(start, end) + endWeight;
     }
 
 }
