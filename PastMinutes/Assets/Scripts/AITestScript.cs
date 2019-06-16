bf6d98c5996b336cf9b7d21704324bffb102747f
Jan W <JanWittke97@web.de>
2019-06-16 23:27:13 +0200

    I hate my life

--- PastMinutes/Assets/Scripts/AITestScript.cs
+++ PastMinutes/Assets/Scripts/AITestScript.cs
@@ -12,20 +12,18 @@ public class AITestScript : MonoBehaviour
     bool move;
     public float speed;
     public Tilemap t;
+    Vector3 start;
 
     // Start is called before the first frame update
     void Start()
     {
         move = false;
-        Vector3Int playerPos = layout.WorldToCell(new Vector3(70, 51, -1.5f));
-        Vector3Int layerPos = layout.WorldToCell(new Vector3(72, 60, -1.5f));
-        
-        Vector3Int emptyPos = layout.WorldToCell(new Vector3(90, 60, 0));
         AStarAlgorithm a = new AStarAlgorithm(layout, map, colliders);
         path = a.FindPath(new Vector3(90, 60, 0), new Vector3(0, 0, 0));
         Vector3 goal = path[0];
         goal.z = transform.position.z;
         transform.position = goal;
+        start = goal;
         path.RemoveAt(0);
         move = true;
     }
@@ -41,33 +39,57 @@ public class AITestScript : MonoBehaviour
 
     public void MoveToPosition()
     {
+        Vector3 goal = NextPoint();
+        float distance = (goal - transform.position).magnitude;
+        float walkingDistance = speed * Time.deltaTime;
+        ////Vector3 direction = new Vector3(path[0].x - npc.transform.position.x, path[0].y - npc.transform.position.y, 0);
+        //if (distance < walkingDistance)
+        //{
+        //    float remaining = walkingDistance - distance;
+        //    MoveToPosition(goal, distance);
+        //    goal = NextPoint();
+        //    if (move)
+        //    {
+        //        Debug.Log("Split Movement");
+        //        MoveToPosition(goal, remaining);
+        //    }
+            
+        //}
+        //else
+       // {
+            MoveToPosition(goal, walkingDistance);
+        //}
+
+    }
+
+    public Vector3 NextPoint()
+    {
         Vector3 goal = path[0];
         goal.z = transform.position.z;
         if (Mathf.Abs(goal.x - transform.position.x) < 0.1f && Mathf.Abs(goal.y - transform.position.y) < 0.1f)
         {
-            Debug.Log("Pos Reached");
+            start = goal;
             path.RemoveAt(0);
-            if(path.Count == 0)
+            if (path.Count == 0)
             {
                 move = false;
-                return;
+                return new Vector3();
             }
             goal = path[0];
             goal.z = transform.position.z;
+            
         }
-        float distance = 0f;
-        float walkingDistance = speed * Time.deltaTime;
-        //Vector3 direction = new Vector3(path[0].x - npc.transform.position.x, path[0].y - npc.transform.position.y, 0);
-        //if(distance < walkingDistance)
-        //{
-        //    float remaining = walkingDistance - distance;
-        //}
-        //else
-        //{
-            transform.position = Vector3.Lerp(transform.position, goal, walkingDistance);
-        //}
-        
+        return goal;
+    }
 
-
+    public void MoveToPosition(Vector3 goal, float distance)
+    {
+        transform.position = Vector3.Lerp(transform.position, goal, distance);
+        //var i = 0.0;
+        //var rate = 1.0 / Time.time;
+        //while (i < 1.0)
+        //{
+        //    i += Time.deltaTime * rate;
+        //    transform.position = Vector3.Lerp(transform.position, endPos, i);
     }
 }
