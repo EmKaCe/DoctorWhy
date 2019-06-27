e18a7144cb2292db137f17b102024b5b58aa8e73
codeisjustlikemagic <codeisjustlikemagic@web.de>
2019-06-28 01:08:25 +0200

    working prerequisite doors. activateObjektAction, StartEndAction?

---
+++ PastMinutes/Assets/Scripts/DialogTree/Dialogs/ActivateObjektAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "ActivateObjektAction", menuName = "NPCAction/ActivateObjektAction")]
+public class ActivateObjektAction : NPCAction
+{
+    public GameObject obj;
+    public override void Act()
+    {
+        Instantiate(obj.gameObject).SetActive(true);
+    }
+}
---
+++ PastMinutes/Assets/Scripts/DialogTree/Dialogs/StartEndAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "StartEndAction", menuName = "NPCAction/StartEndAction")]
+public class StartEndAction : NPCAction
+{
+    public override void Act() {
+        Debug.Log("Shila: Tanz für mich terraformer, tanz!");
+        EventManager.TriggerEvent(EventSystem.StartWorldEnd(), 0, new string[] { });
+    }
+    
+}
