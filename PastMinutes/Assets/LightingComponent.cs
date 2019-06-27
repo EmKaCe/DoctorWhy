67d6f5bf230d0b59c35c68c441074c971b9c4bfd
Jan W <JanWittke97@web.de>
2019-06-27 14:56:27 +0200

    Added TestLightingScene

---
+++ PastMinutes/Assets/LightingComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.ambientLight = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
