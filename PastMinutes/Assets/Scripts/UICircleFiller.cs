16deb972cacac7a21478a5133924ee5fa2db9041
Jan W <JanWittke97@web.de>
2019-06-28 13:46:00 +0200

    Removed time gauntlet and pistol from zombies

---
+++ PastMinutes/Assets/Scripts/UICircleFiller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICircleFiller : MonoBehaviour
+{
+
+    public Image fillingCircle;
+    public float fillingValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(fillingCircle == null)
+        {
+            Debug.Log("Circle to fill not set. Please add gameObject to fillingCircle.");
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fillingCircle.fillAmount = fillingValue;
+    }
+}
