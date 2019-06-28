bd63308f2f78d09ef4f6cb8a24e12a2978faafa4
Emre Kaan Çetin <20030146+EmKaCe@users.noreply.github.com>
2019-06-28 02:00:01 +0200

    Fixed UI Bars, Added OxygenContainer, Changes to Inventory (nvm them)

---
+++ PastMinutes/Assets/Scripts/UI/SlotPanelUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotPanelUI : MonoBehaviour
+{
+    public GameObject SlotPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 12; i++)
+        {
+            Instantiate(SlotPrefab).transform.parent = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
